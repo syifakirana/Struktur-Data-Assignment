d1f00f162b51f39a79c098629272102e3a474e0e
syifakirana <syifa.kiranaps@gmail.com>
2025-10-01 22:58:33 +0700

    Add files via upload

---
+++ MODUL 1 - Pengenalan Bahasa C++ (1)/guided/operator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+
+    cout << "Masukkan angka1: ";
+    cin >> a;
+    cout << "Masukkan angka2: ";
+    cin >> b;
+
+    //Operator aritmatika
+    cout << "a + b = " << (a+b) << endl;
+    cout << "a - b = " << (a-b) << endl;
+    cout << "a * b = " << (a*b) << endl;
+    cout << "a / b = " << (a/b) << endl;
+    cout << "a % b = " << (a%b) << endl;
+
+    cout << "a > b = " << (a>b) << endl;
+    cout << "a < b = " << (a<b) << endl;
+    cout << "a >= b = " << (a>=b) << endl;
+    cout << "a <= b = " << (a<=b) << endl;
+    cout << "a == b = " << (a==b) << endl;
+    cout << "a != b = " << (a!=b) << endl;
+    return 0;
+}
