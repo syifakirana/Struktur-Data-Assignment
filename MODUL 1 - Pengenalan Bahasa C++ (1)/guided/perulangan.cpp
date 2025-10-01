41d9685308c08c4602f7d52ecec9f2d04c5e72a9
syifakirana <syifa.kiranaps@gmail.com>
2025-10-01 23:04:51 +0700

    Add files via upload

---
+++ MODUL 1 - Pengenalan Bahasa C++ (1)/guided/perulangan.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int i ;
+   int j ;
+
+   for (int i = 0; i < 10; i++) {
+         cout << i <<  "-";
+      }
+      cout << endl;
+
+    do {
+        cout << j << "-";
+        j++;
+    } while (j < 10);
+
+    return 0;
+   }
