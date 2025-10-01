32bc97191fbaf901ad1cd5a9c7e0dd240a3eea26
syifakirana <syifa.kiranaps@gmail.com>
2025-10-01 23:08:59 +0700

    Add files via upload

---
+++ MODUL 1 - Pengenalan Bahasa C++ (1)/Unguided/soal1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     float a, b;
+
+    cout << "Masukkan bilangan pertama: ";
+    cin >> a;
+    cout << "Masukkan bilangan kedua: ";
+    cin >> b;
+
+    //Operator aritmatika
+    cout << "a + b = " << (a+b) << endl;
+    cout << "a - b = " << (a-b) << endl;
+    cout << "a * b = " << (a*b) << endl;
+    cout << "a / b = " << (a/b) << endl;
+
+    return 0;
+}
