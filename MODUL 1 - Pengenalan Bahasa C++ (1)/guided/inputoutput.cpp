9906338a727cbe73e3b308e4cb1088d4eefc01df
syifakirana <syifa.kiranaps@gmail.com>
2025-10-01 22:59:34 +0700

    Add files via upload

---
+++ MODUL 1 - Pengenalan Bahasa C++ (1)/guided/inputoutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    const float pi = 3.14;
+
+    cout << "Masukkan angka: ";
+    cin >> n;
+
+    cout << "Angka dikeluarkan: " << n << endl;
+    cout << "Nilai konstanta pi: " << pi << endl;
+    return 0;
+    
+}
