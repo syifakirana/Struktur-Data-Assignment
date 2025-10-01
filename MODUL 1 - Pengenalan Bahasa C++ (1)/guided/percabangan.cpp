422d8f1ba3067b576cca3c1b37a41c84aa67fefa
syifakirana <syifa.kiranaps@gmail.com>
2025-10-01 23:02:04 +0700

    Add files via upload

---
+++ MODUL 1 - Pengenalan Bahasa C++ (1)/guided/percabangan.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int angka1 = 10;
+    int angka2 = 20;
+
+    if (angka1 > angka2) {
+        cout << "Angka 1 lebih besar dari Angka 2" << endl;
+    } else if (angka1 < angka2) {
+        cout  << "Angka 1 lebih kecil dari Angka 2" << endl;
+    } else {
+        cout << "Angka 1 sama dengan Angka 2" << endl;
+    }
+
+    return 0;
+    
+}
