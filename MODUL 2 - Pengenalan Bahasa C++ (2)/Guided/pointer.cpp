ed2cf468eb7cb042effc0d2e89f60744b9f8afb1
syifakirana <syifa.kiranaps@gmail.com>
2025-10-07 13:32:33 +0700

    Add files via upload

---
+++ MODUL 2 - Pengenalan Bahasa C++ (2)/Guided/pointer.cpp
@@ -0,0 +1,28 @@
+//Pointer
+#include <iostream>
+using namespace std;
+
+void tukar(int *x, int *y) {
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+} 
+
+int main() {
+    int a =20, b =30;
+    int *ptr;
+
+    ptr = &a;
+
+    cout << "Value of a: " << a << endl;
+    cout << "Address of a: " << &a << endl;
+    cout << "Value stored in ptr (Address of a): " << ptr << endl;
+    cout << "Value pointes to by ptr: " << *ptr << endl;
+
+    tukar(&a, &b);
+    cout << "After swapping, value of a = " << a << " and b = " << b << endl;
+
+    return 0;
+    
+}
