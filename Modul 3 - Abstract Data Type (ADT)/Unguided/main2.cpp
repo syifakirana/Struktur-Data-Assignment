523c0c58f584157a22fa923902f85037276faa51
syifakirana <syifa.kiranaps@gmail.com>
2025-10-16 21:38:42 +0700

    Add files via upload

---
+++ Modul 3 - Abstract Data Type (ADT)/Unguided/main2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "pelajaran.h"
+using namespace std;
+
+int main() {
+    string namapel = "Struktur Data";
+    string kodepel = "STD";
+
+    pelajaran pel = create_pelajaran(namapel, kodepel);
+    tampil_pelajaran(pel);
+
+    return 0;
+}
