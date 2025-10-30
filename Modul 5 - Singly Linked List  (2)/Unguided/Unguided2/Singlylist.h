71002020506ad2a53705aff756554435eb1ed1b2
syifakirana <syifa.kiranaps@gmail.com>
2025-10-30 22:40:59 +0700

    Add files via upload

---
+++ Modul 5 - Singly Linked List  (2)/Unguided/Unguided2/Singlylist.h
@@ -0,0 +1,29 @@
+#ifndef SINGLYLIST_H_INCLUDED
+#define SINGLYLIST_H_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+// deklarasi tipe data dasar
+typedef int infotype;
+typedef struct ElmtList *address;
+
+//struktur elemen list
+struct ElmtList {
+    infotype info;
+    address next;
+};
+
+// struktur list
+struct List {
+    address first;
+};
+
+// deklarasi function dan procedure
+void createList(List &L);
+address alokasi(infotype x);
+void dealokasi(address &P);
+void insertFirst(List &L, address P);
+void printInfo(List L);
+
+#endif // SINGLYLIST_H_INCLUDED
