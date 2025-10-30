7a70ab62969b5ac872e50b31f2993e455d286121
syifakirana <syifa.kiranaps@gmail.com>
2025-10-30 22:45:12 +0700

    Add files via upload

---
+++ Modul 5 - Singly Linked List  (2)/Unguided/Unguided4/main.cpp
@@ -0,0 +1,33 @@
+#include "SinglyList.h"
+
+int main() {
+    List L;
+    address P1, P2, P3, P4, P5;
+
+    // inisialisasi list kosong
+    createList(L);
+
+    // menambahkan elemen ke list
+    P1 = alokasi(2);
+    insertFirst(L, P1);
+
+    P2 = alokasi(0);
+    insertFirst(L, P2);
+
+    P3 = alokasi(8);
+    insertFirst(L, P3);
+
+    P4 = alokasi(12);
+    insertFirst(L, P4);
+
+    P5 = alokasi(9);
+    insertFirst(L, P5);
+
+    // menampilkan isi list
+    cout << "Isi Linked List: ";
+    printInfo(L);
+
+    int total = sumElm(L); //tambahan
+    cout << "Total info dari kelima elemen adalah " << total << endl;
+    return 0;
+}
