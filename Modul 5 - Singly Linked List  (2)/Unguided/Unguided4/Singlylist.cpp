12580d2dddd22c5d668a054ec055104b1f846bfa
syifakirana <syifa.kiranaps@gmail.com>
2025-10-30 22:45:32 +0700

    Add files via upload

---
+++ Modul 5 - Singly Linked List  (2)/Unguided/Unguided4/Singlylist.cpp
@@ -0,0 +1,50 @@
+#include "SinglyList.h"
+
+// buat list kosong
+void createList(List &L) {
+    L.first = NULL;
+}
+
+// alokasi node baru
+address alokasi(infotype x) {
+    address P = new ElmtList;
+    P->info = x;
+    P->next = NULL;
+    return P;
+}
+
+// hapus alokasi node dari memori
+void dealokasi(address &P) {
+    delete P;
+    P = NULL;
+}
+
+// nambah elemen di awal list
+void insertFirst(List &L, address P) {
+    if (L.first == NULL) {
+        L.first = P;
+    } else {
+        P->next = L.first;
+        L.first = P;
+    }
+}
+
+// menampilkan semua isi list
+void printInfo(List L) {
+    address P = L.first;
+    while (P != NULL) {
+        cout << P->info << " ";
+        P = P->next;
+    }
+    cout << endl;
+}
+
+int sumElm(List L) { //tambahan
+    int total = 0;
+    address P = L.first;
+    while (P != NULL) {
+        total += P->info;
+        P = P->next;
+    }
+    return total;
+}
