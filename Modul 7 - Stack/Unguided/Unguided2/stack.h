6cb08d1f385d713774a08c286dae627cc56cffd5
syifakirana <syifa.kiranaps@gmail.com>
2025-11-17 02:13:38 +0700

    Add files via upload

---
+++ Modul 7 - Stack/Unguided/Unguided2/stack.h
@@ -0,0 +1,23 @@
+#ifndef STACK_H_INCLUDED
+#define STACK_H_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+typedef int infotype;
+
+struct Stack {
+    infotype info[20];
+    int top;
+};
+
+void CreateStack(Stack &S);
+void Push(Stack &S, infotype x);
+infotype Pop(Stack &S);
+void printInfo(Stack S);
+void balikStack(Stack &S);
+
+void pushAscending(Stack &S, infotype x); //tambahan
+
+
+#endif
