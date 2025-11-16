b2a9ea726836027919cbc458981308f65cd5065f
syifakirana <syifa.kiranaps@gmail.com>
2025-11-16 23:06:12 +0700

    Add files via upload

---
+++ Modul 7 - Stack/Unguided/Unguided1/main.cpp
@@ -0,0 +1,26 @@
+#include "stack.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world!" << endl;
+
+    Stack S;
+    CreateStack(S);
+
+    Push(S, 3);
+    Push(S, 4);
+    Push(S, 2);
+    Push(S, 9);
+    Push(S, 8);
+
+    Pop(S);       // hapus 8
+
+    printInfo(S); // hasil sebelum balik
+    cout << "balik stack" << endl;
+
+    balikStack(S);
+    printInfo(S);
+
+    return 0;
+}
