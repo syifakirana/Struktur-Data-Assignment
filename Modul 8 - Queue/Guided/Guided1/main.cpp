47cbc82f886757ebca155fede4ce37308954fbe0
syifakirana <syifa.kiranaps@gmail.com>
2025-11-17 08:03:08 +0700

    Add files via upload

---
+++ Modul 8 - Queue/Guided/Guided1/main.cpp
@@ -0,0 +1,32 @@
+// guided 1 main.cpp
+#include "queue.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    queue Q;
+    CreateQueue(Q);
+
+    enQueue(Q, "dhimas");
+    enQueue(Q, "Arvin");
+    enQueue(Q, "Rizal");
+    enQueue(Q, "Hafizh");
+    enQueue(Q, "Fathur");
+    enQueue(Q, "Atha");
+    cout << endl;
+
+    cout << "--- Isi Queue Setelah enQueue ---" << endl;
+    viewQueue(Q);
+
+    deQueue(Q);
+    deQueue(Q);
+    deQueue(Q);
+    deQueue(Q);
+    cout << endl;
+
+    cout << "--- Isi Queue Setelah deQueue ---" << endl;
+    viewQueue(Q);
+
+    clearQueue(Q);
+    return 0;
+}
