ff5bf7cb76c7f992b1d4b0a229c7e86c2aff3496
syifakirana <syifa.kiranaps@gmail.com>
2025-11-18 21:22:27 +0700

    Add files via upload

---
+++ Modul 8 - Queue/Unguided/Unguided2/queue.h
@@ -0,0 +1,22 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <iostream>
+using namespace std;
+
+const int MAKSIMAL = 5;
+
+struct Queue {
+    int info[MAKSIMAL];
+    int head;
+    int tail;
+};
+
+void createQueue(Queue &Q);
+bool isEmpty(Queue Q);
+bool isFull(Queue Q);
+void enqueue(Queue &Q, int x);
+int dequeue(Queue &Q);
+void printInfo(Queue Q);
+
+#endif
