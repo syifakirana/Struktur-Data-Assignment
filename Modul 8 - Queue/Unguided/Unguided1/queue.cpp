41dd44b281bd3b2154cf1e7527c7054d1fa8d0c8
syifakirana <syifa.kiranaps@gmail.com>
2025-11-18 21:20:29 +0700

    Add files via upload

---
+++ Modul 8 - Queue/Unguided/Unguided1/queue.cpp
@@ -0,0 +1,70 @@
+#include "queue.h"
+#include <iostream>
+using namespace std;
+
+void createQueue(Queue &Q) {
+    Q.head = -1;
+    Q.tail = -1;
+}
+
+bool isEmpty(Queue Q) {
+    return (Q.head == -1 && Q.tail == -1);
+}
+
+bool isFull(Queue Q) {
+    return (Q.tail == MAKSIMAL - 1);
+}
+
+void enqueue(Queue &Q, int x) {
+    if (isFull(Q)) {
+        cout << "Queue penuh!" << endl;
+        return;
+    }
+    if (isEmpty(Q)) {
+        Q.head = 0;
+        Q.tail = 0;
+    } else {
+        Q.tail++;
+    }
+    Q.info[Q.tail] = x;
+}
+
+int dequeue(Queue &Q) {
+    if (isEmpty(Q)) {
+        cout << "Queue kosong!" << endl;
+        return -1;
+    }
+
+    int nilai = Q.info[Q.head];
+
+    // geser semua elemen ke kiri
+    for (int i = 0; i < Q.tail; i++) {
+        Q.info[i] = Q.info[i + 1];
+    }
+
+    Q.tail--;
+
+    // jika kosong reset
+    if (Q.tail < 0) {
+        Q.head = -1;
+        Q.tail = -1;
+    }
+
+    return nilai;
+}
+
+void printInfo(Queue Q) {
+    if (isEmpty(Q)) {
+        cout << Q.head << "  -  " << Q.tail << "    | empty queue" << endl;
+        return;
+    }
+
+    cout << Q.head << "  -  " << Q.tail << "    | ";
+
+    for (int i = Q.head; i <= Q.tail; i++) {
+        cout << Q.info[i];
+        if (i < Q.tail) cout << " ";
+    }
+
+    cout << endl;
+}
