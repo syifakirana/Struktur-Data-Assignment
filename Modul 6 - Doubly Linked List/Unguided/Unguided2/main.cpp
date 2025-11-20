c5103c70b13b875a9ca655e26ab931780c73df63
syifakirana <syifa.kiranaps@gmail.com>
2025-11-20 20:14:13 +0700

    Add files via upload

---
+++ Modul 6 - Doubly Linked List/Unguided/Unguided2/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include "Doublylist.h"
+using namespace std;
+
+int main() {
+    List L;
+    CreateList(L);
+
+    const int JUMLAH_INPUT = 4;
+    for (int i = 0; i < JUMLAH_INPUT; ++i) {
+        infotipe x;
+
+        cout << "masukkan nomor polisi: ";
+        cin >> x.nopol;
+
+        cout << "masukkan warna kendaraan: ";
+        cin >> x.warna;
+
+        cout << "masukkan tahun kendaraan: ";
+        cin >> x.thnBuat;
+
+        cout << endl;
+
+        // cek duplikat
+        bool exists = false;
+        for (address Q = L.First; Q != nullptr; Q = Q->next) {
+            if (Q->info.nopol == x.nopol) {
+                exists = true;
+                break;
+            }
+        }
+
+        if (exists) {
+            cout << "nomor polisi sudah terdaftar" << endl;
+            cout << endl;
+        } else {
+            address P = alokasi(x);
+            insertLast(L, P);
+        }
+    }
+
+    // Tampilkan isi list
+    printInfo(L);
+    
+    //tambahan
+    string cari;
+    cout << "Masukkan Nomor Polisi yang dicari : ";
+    cin >> cari;
+
+    address hasil = findElm(L, cari);
+    cout << endl;
+
+    if (hasil != nullptr) {
+        cout << "Nomor Polisi : " << hasil->info.nopol << endl;
+        cout << "Warna        : " << hasil->info.warna << endl;
+        cout << "Tahun        : " << hasil->info.thnBuat << endl;
+    } else {
+        cout << "Data tidak ditemukan!" << endl;
+    }
+
+    address curr = L.First;
+    while (curr != nullptr) {
+        address toDel = curr;
+        curr = curr->next;
+        dealokasi(toDel);
+    }
+    L.First = L.Last = nullptr;
+
+    return 0;
+}
