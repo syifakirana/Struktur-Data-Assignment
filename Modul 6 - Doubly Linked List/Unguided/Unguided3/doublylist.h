560ccc4911e3b6c6106dbddf8bcf893d283c835c
syifakirana <syifa.kiranaps@gmail.com>
2025-11-20 20:17:04 +0700

    Add files via upload

---
+++ Modul 6 - Doubly Linked List/Unguided/Unguided3/doublylist.h
@@ -0,0 +1,41 @@
+#ifndef DOUBLYLIST_H
+#define DOUBLYLIST_H
+
+#include <string>
+using namespace std;
+
+struct infotipe {
+    string nopol;
+    string warna;
+    int thnBuat;
+};
+
+struct ElmList;
+typedef ElmList* address;
+
+struct ElmList {
+    infotipe info;
+    address next;
+    address prev;
+};
+
+struct List {
+    address First;
+    address Last;
+};
+
+void CreateList(List &L);
+address alokasi(infotipe x);
+void dealokasi(address P);
+void insertLast(List &L, address P);
+void printInfo(List L);
+
+// soal 2
+address findElm(List L, string nopol);
+
+// soal 3 (delete)
+void deleteFirst(List &L, address &P);
+void deleteLast(List &L, address &P);
+void deleteAfter(address Prec, address &P);
+
+#endif
