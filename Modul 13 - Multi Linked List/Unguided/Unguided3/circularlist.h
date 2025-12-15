9480a20d5ee647f86c96126167fd60dcae450846
syifakirana <syifa.kiranaps@gmail.com>
2025-12-15 15:52:24 +0700

    Add files via upload

---
+++ Modul 13 - Multi Linked List/Unguided/Unguided3/circularlist.h
@@ -0,0 +1,44 @@
+#ifndef CIRCULARLIST_H_INCLUDED
+#define CIRCULARLIST_H_INCLUDED
+#include <iostream>
+#include <string>
+using namespace std;
+#define Nil NULL
+
+typedef struct mahasiswa {
+    string nama;
+    string nim;
+    char jenis_kelamin;
+    float ipk;
+} infotype;
+
+typedef struct ElmList *address;
+
+struct ElmList {
+    infotype info;
+    address next;
+};
+
+struct List {
+    address First;
+};
+
+/* 11 FUNGSI SESUAI MODUL */
+void createList(List &L);
+address alokasi(infotype x);
+void dealokasi(address &P);
+
+void insertFirst(List &L, address P);
+void insertAfter(List &L, address Prec, address P);
+void insertLast(List &L, address P);
+
+void deleteFirst(List &L, address &P);
+void deleteAfter(List &L, address Prec, address &P);
+void deleteLast(List &L, address &P);
+
+address findElm(List L, infotype x);
+void printInfo(List L);
+
+address createData(string nama, string nim, char jenis_kelamin, float ipk);
+
+#endif
